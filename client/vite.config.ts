1ba6304b946290591e7e71feff1175fe278d3eb2
Munawirul Hadi <hadi.munawirul@gmail.com>
2023-03-01 19:33:48 +0700

    Fix manifest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
           registerType: 'prompt',  
           includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],  
           manifest: {  
-      name: 'ABI-AI',
-display: standalone, 
+            name: 'ABI-AI',
+            display: "standalone", 
             short_name: 'ABI AI',  
             description: 'An AI Chatbot based on GPT-3 API',  
             theme_color: '#ffffff',  
